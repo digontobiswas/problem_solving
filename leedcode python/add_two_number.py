7692dc8c05d50d11fff3ab3e38806ea105ac6fcf
digontobiswas <digontobiswas.kp.bd.com>
2025-01-07 08:56:29 +0530

    leed code add two number done

---
+++ leedcode python/add_two_number.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/add-two-numbers/submissions/1500212686/
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        def toint(node):
+            return node.val + 10 * toint(node.next) if node else 0
+        def tolist(n):
+            node = ListNode(n % 10)
+            if n > 9:
+                node.next = tolist(n / 10)
+            return node
+        return tolist(toint(l1) + toint(l2))
